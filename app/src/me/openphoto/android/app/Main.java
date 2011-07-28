5f558fdb621a9ad3188ec779c5a90f78308a566d
Peter Sankauskas <pas256@gmail.com>
2011-07-27 22:41:42 -0700

    Initial commit of a new and empty Android project using Eclipse

---
+++ app/src/me/openphoto/android/app/Main.java
@@ -0,0 +1,13 @@
+package me.openphoto.android.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
