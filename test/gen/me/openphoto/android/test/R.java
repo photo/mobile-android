5941a78427825f69da7094a8a1bfb8b3135cc960
Peter Sankauskas <pas256@gmail.com>
2011-07-27 23:25:00 -0700

    Adding an ID to the TextView in Main, and adding the first Android Testing unit tests

--- test/gen/me/openphoto/android/test/R.java
+++ test/gen/me/openphoto/android/test/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package me.openphoto.android.app;
+package me.openphoto.android.test;
 
 public final class R {
     public static final class attr {
@@ -13,13 +13,11 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
-    public static final class id {
-        public static final int title=0x7f050000;
-    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
+        public static final int app_name=0x7f040001;
+        public static final int hello=0x7f040000;
     }
 }
