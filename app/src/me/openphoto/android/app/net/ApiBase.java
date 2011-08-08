825c6c80bf3a768714d40992fa6da9be907eaad9
Patrick Boos <pboos@tonchidot.com>
2011-08-08 12:44:06 +0900

    let user execute a ApiRequest to receive an ApiResponse

--- app/src/me/openphoto/android/app/net/ApiBase.java
+++ app/src/me/openphoto/android/app/net/ApiBase.java
@@ -1,6 +1,22 @@
 
 package me.openphoto.android.app.net;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.HTTP;
+
 /**
  * ApiBase provides the basic functionality to call RESTful APIs using an
  * ApiRequest.
@@ -17,5 +33,60 @@ public class ApiBase {
 
     public String getApiUrl() {
         return mBaseUrl;
+    }
+
+    public ApiResponse execute(ApiRequest request) throws ClientProtocolException, IOException {
+        DefaultHttpClient httpClient = new DefaultHttpClient();
+        HttpUriRequest httpRequest = createHttpRequest(request);
+
+        // This is needed because otherwise photo upload can fail on PHP servers
+        httpRequest.getParams().setBooleanParameter("http.protocol.expect-continue", false);
+
+        return new ApiResponse(httpClient.execute(httpRequest));
+    }
+
+    private HttpUriRequest createHttpRequest(ApiRequest request)
+            throws UnsupportedEncodingException {
+        HttpUriRequest httpRequest = null;
+        switch (request.getMethod()) {
+            case ApiRequest.GET:
+                httpRequest = new HttpGet(addGetParams(mBaseUrl + request.getPath(),
+                        request.getParameters()));
+                break;
+            case ApiRequest.POST:
+                httpRequest = new HttpPost(mBaseUrl + request.getPath());
+                ((HttpPost) httpRequest).setEntity(new UrlEncodedFormEntity(
+                        request.getParameters(), HTTP.UTF_8));
+                break;
+            case ApiRequest.PUT:
+                httpRequest = new HttpPut(addGetParams(mBaseUrl + request.getPath(),
+                        request.getParameters()));
+                break;
+            case ApiRequest.DELETE:
+                httpRequest = new HttpDelete(addGetParams(mBaseUrl + request.getPath(),
+                        request.getParameters()));
+                break;
+        }
+
+        for (NameValuePair pair : request.getHeaders()) {
+            request.addHeader(pair.getName(), pair.getValue());
+        }
+
+        return httpRequest;
+    }
+
+    private String addGetParams(String url, List<NameValuePair> nameValuePairs) {
+        if (nameValuePairs == null || nameValuePairs.isEmpty()) {
+            return url;
+        }
+
+        final StringBuffer newUrl = new StringBuffer(url);
+        newUrl.append(url.contains("?") ? '&' : '?');
+
+        for (NameValuePair param : nameValuePairs) {
+            newUrl.append(param.getName() + "=" + param.getValue() + "&");
+        }
+        newUrl.deleteCharAt(newUrl.length() - 1);
+        return newUrl.toString();
     }
 }
--- test/src/me/openphoto/android/test/net/ApiBaseTest.java
+++ test/src/me/openphoto/android/test/net/ApiBaseTest.java
@@ -1,8 +1,16 @@
 
 package me.openphoto.android.test.net;
 
+import java.io.IOException;
+
 import junit.framework.TestCase;
 import me.openphoto.android.app.net.ApiBase;
+import me.openphoto.android.app.net.ApiRequest;
+import me.openphoto.android.app.net.ApiResponse;
+
+import org.apache.http.client.ClientProtocolException;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ApiBaseTest extends TestCase {
     public void testConstructorUrl() {
@@ -14,5 +22,18 @@ public class ApiBaseTest extends TestCase {
         ApiBase api = new ApiBase(OpenPhotoApiConstants.OPENPHOTO_BASE_URI + "/");
         assertEquals("Even if given a slash at the end, this should be removed",
                 OpenPhotoApiConstants.OPENPHOTO_BASE_URI, api.getApiUrl());
+    }
+
+    public void testBasicRequest() throws ClientProtocolException, IOException, JSONException {
+        ApiBase api = new ApiBase(OpenPhotoApiConstants.OPENPHOTO_BASE_URI);
+        ApiRequest request = new ApiRequest(ApiRequest.GET, "/photos.json");
+
+        ApiResponse response = api.execute(request);
+        assertEquals(200, response.getStatusCode());
+
+        String responseString = response.getContentAsString();
+        assertNotNull(responseString);
+        JSONObject json = new JSONObject(responseString);
+        assertTrue(json.has("message"));
     }
 }
