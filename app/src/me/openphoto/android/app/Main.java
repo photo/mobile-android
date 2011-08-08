45560602970c3f3529fbcf74a458befb1782e49a
Patrick Boos <pboos@tonchidot.com>
2011-08-08 18:26:22 +0900

    show newest picture in the first activity

--- app/src/me/openphoto/android/app/Main.java
+++ app/src/me/openphoto/android/app/Main.java
@@ -1,10 +1,21 @@
 /**
  * The Main screen of OpenPhoto
  */
+
 package me.openphoto.android.app;
 
+import java.net.URL;
+
+import me.openphoto.android.app.model.Photo;
+import me.openphoto.android.app.net.OpenPhotoApi;
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 /**
  * The Main screen of OpenPhoto
@@ -12,14 +23,43 @@ import android.os.Bundle;
  * @author pas
  */
 public class Main extends Activity {
-	/**
-	 * Called when Main Activity is first loaded
-	 * 
-	 * @see android.app.Activity#onCreate(android.os.Bundle)
-	 */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-	}
+    /**
+     * Called when Main Activity is first loaded
+     * 
+     * @see android.app.Activity#onCreate(android.os.Bundle)
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        findViewById(R.id.image).setVisibility(View.GONE);
+        new LoadImageTask().execute();
+    }
+
+    private class LoadImageTask extends AsyncTask<Void, Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(Void... params) {
+            OpenPhotoApi api = new OpenPhotoApi("http://current.openphoto.me");
+            try {
+                Photo photo = api.getPhotos().getPhotos().get(0);
+                return BitmapFactory.decodeStream(new URL(photo.getUrl("640x960")).openStream());
+            } catch (Exception e) {
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            if (result != null) {
+                findViewById(R.id.progress).setVisibility(View.GONE);
+                ImageView image = (ImageView) findViewById(R.id.image);
+                image.setImageBitmap(result);
+                image.setVisibility(View.VISIBLE);
+            } else {
+                Toast.makeText(Main.this, "Could not download image", Toast.LENGTH_LONG).show();
+            }
+        }
+
+    }
 }
