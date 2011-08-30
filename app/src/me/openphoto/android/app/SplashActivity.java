13080a80d5d28c677025555dab827ab6d03b029b
Patrick Boos <pboos@tonchidot.com>
2011-08-30 13:38:25 +0900

    do not show splash screen (since not yet used), if showing splash screen, if closed, do not move on to MainActivity
    This as well fixes some bugs in the tests that need to check what is on the screen + mocks

--- app/src/me/openphoto/android/app/SplashActivity.java
+++ app/src/me/openphoto/android/app/SplashActivity.java
@@ -1,6 +1,7 @@
 /**
  * The splash/loading screen before you get to the Main screen
  */
+
 package me.openphoto.android.app;
 
 import android.app.Activity;
@@ -14,53 +15,61 @@ import android.os.Bundle;
  * @author pas
  */
 public class SplashActivity extends Activity {
-	private InitialLoad loadTask;
+    private InitialLoad loadTask;
 
-	/**
-	 * Called when Splash Activity is first loaded
-	 * 
-	 * @see android.app.Activity#onCreate(android.os.Bundle)
-	 */
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.splash);
+    /**
+     * Called when Splash Activity is first loaded
+     * 
+     * @see android.app.Activity#onCreate(android.os.Bundle)
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
 
-		loadTask = new InitialLoad();
-		loadTask.execute((Void) null);
-	}
+        loadTask = new InitialLoad();
+        loadTask.execute((Void) null);
+    }
 
-	/**
-	 * Async task to do whatever loading will be required before going to the
-	 * Main screen
-	 */
-	private class InitialLoad extends AsyncTask<Void, Void, Void> {
-		/**
-		 * @see android.os.AsyncTask#doInBackground(Params[])
-		 */
-		@Override
-		protected Void doInBackground(Void... params) {
-			// TODO Remove this fake work and replace with real work
-			try {
-				Thread.sleep(2000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			return null;
-		}
+    @Override
+    protected void onPause() {
+        loadTask.cancel(true);
+        super.onPause();
+    }
 
-		/**
-		 * @see android.os.AsyncTask#onPostExecute(java.lang.Object)
-		 */
-		@Override
-		protected void onPostExecute(Void result) {
-			super.onPostExecute(result);
+    /**
+     * Async task to do whatever loading will be required before going to the
+     * Main screen
+     */
+    private class InitialLoad extends AsyncTask<Void, Void, Void> {
+        /**
+         * @see android.os.AsyncTask#doInBackground(Params[])
+         */
+        @Override
+        protected Void doInBackground(Void... params) {
+            // TODO Remove this fake work and replace with real work
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            return null;
+        }
 
-			// Go to Main screen
-			Intent i = new Intent(SplashActivity.this, MainActivity.class);
-			startActivity(i);
-			finish();
-		}
-	}
+        /**
+         * @see android.os.AsyncTask#onPostExecute(java.lang.Object)
+         */
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+
+            if (!isCancelled()) {
+                // Go to Main screen
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }
+    }
 }
