17505f13b15dbe5687299df7445f4e701b57d721
Patrick Boos <patrickboos85@gmail.com>
2012-02-21 15:18:07 +0900

    Add text to the tabs

--- app/src/me/openphoto/android/app/MainActivity.java
+++ app/src/me/openphoto/android/app/MainActivity.java
@@ -16,6 +16,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TabHost.TabSpec;
+import android.widget.TextView;
 
 /**
  * The Main screen of OpenPhoto
@@ -51,24 +52,25 @@ public class MainActivity extends TabActivity implements ActionClickListener {
 
     private void setUpTabs() {
         TabSpec tabSpec = getTabHost().newTabSpec("home")
-                .setIndicator(newTabIndicator(R.drawable.tab_home))
+                .setIndicator(newTabIndicator(R.drawable.tab_home, R.string.tab_home))
                 .setContent(new Intent(this, HomeActivity.class));
         getTabHost().addTab(tabSpec);
 
         tabSpec = getTabHost().newTabSpec("gallery")
-                .setIndicator(newTabIndicator(R.drawable.tab_gallery))
+                .setIndicator(newTabIndicator(R.drawable.tab_gallery, R.string.tab_gallery))
                 .setContent(new Intent(this, GalleryActivity.class));
         getTabHost().addTab(tabSpec);
 
         tabSpec = getTabHost().newTabSpec("tags")
-                .setIndicator(newTabIndicator(R.drawable.tab_tags))
+                .setIndicator(newTabIndicator(R.drawable.tab_tags, R.string.tab_tags))
                 .setContent(new Intent(this, TagsActivity.class));
         getTabHost().addTab(tabSpec);
     }
 
-    private View newTabIndicator(int drawableResId) {
+    private View newTabIndicator(int drawableResId, int textResId) {
         View view = getLayoutInflater().inflate(R.layout.tab, null);
         ((ImageView) view.findViewById(R.id.image)).setImageResource(drawableResId);
+        ((TextView) view.findViewById(R.id.text)).setText(textResId);
         return view;
     }
 
