bcc0bf62d1cecf3dfbe3c7b2196c02070f7503fc
Patrick Santana <patricksan@me.com>
2012-08-09 12:00:20 +0200

    adding sha1 utility class

---
+++ app/src/me/openphoto/android/app/util/SHA1Utils.java
@@ -0,0 +1,36 @@
+package me.openphoto.android.app.util;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * @author Patrick Santana <patrick@openphoto.me>
+ * 
+ */
+public class SHA1Utils {
+
+	private static String convertToHex(byte[] data) {
+		StringBuilder buf = new StringBuilder();
+		for (int i = 0; i < data.length; i++) {
+			int halfbyte = (data[i] >>> 4) & 0x0F;
+			int two_halfs = 0;
+			do {
+				if ((0 <= halfbyte) && (halfbyte <= 9))
+					buf.append((char) ('0' + halfbyte));
+				else
+					buf.append((char) ('a' + (halfbyte - 10)));
+				halfbyte = data[i] & 0x0F;
+			} while (two_halfs++ < 1);
+		}
+		return buf.toString();
+	}
+
+	public static String SHA1(String text) throws NoSuchAlgorithmException,
+			UnsupportedEncodingException {
+		MessageDigest md = MessageDigest.getInstance("SHA-1");
+		md.update(text.getBytes("iso-8859-1"), 0, text.length());
+		byte[] sha1hash = md.digest();
+		return convertToHex(sha1hash);
+	}
+}
