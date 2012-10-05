4447a21c1bbb6f8b851a3f5ad3a942924134064c
Eugene Popovich <httpdispatch@gmail.com>
2012-10-05 15:26:50 +0300

    #5 - removed reference to NetworkAccessControl. Now Utils.isOnline method is used instead in the CommonFragment - removed implementation of NetworkAccessControl from the MainActivity - added isWiFiOnlyUploadActive method to the Preferences - removed isOnline method. Now Utils.isOnline is used instead of in the UploaderService - removed deprecated Notification creation. Now NotificationCompat.Builder is used instead of in the UploaderService - added wifi only upload preference check to the uploading functionality in the UploaderService - added new methods isOnline and isWiFiActive to the Utils

--- app/src/me/openphoto/android/app/service/UploaderService.java
+++ app/src/me/openphoto/android/app/service/UploaderService.java
@@ -16,6 +16,7 @@ import me.openphoto.android.app.net.UploadResponse;
 import me.openphoto.android.app.provider.PhotoUpload;
 import me.openphoto.android.app.provider.UploadsProviderAccessor;
 import me.openphoto.android.app.util.ImageUtils;
+import me.openphoto.android.app.util.Utils;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -31,10 +32,21 @@ import android.os.HandlerThread;
 import android.os.IBinder;
 import android.os.Looper;
 import android.os.Message;
+import android.support.v4.app.NotificationCompat;
 import android.text.TextUtils;
 import android.util.Log;
 import android.widget.RemoteViews;
 
+/**
+ * @version
+ *          05.10.2012
+ *          <br>- removed isOnline method. Now Utils.isOnline is used instead of
+ *          <br>- removed deprecated Notification creation. Now
+ *          NotificationCompat.Builder is used instead of
+ *          <br>- added wifi only upload preference check to the uploading
+ *          functionality
+ * 
+ */
 public class UploaderService extends Service {
     private static final int NOTIFICATION_UPLOAD_PROGRESS = 1;
     private static final String TAG = UploaderService.class.getSimpleName();
@@ -96,9 +108,12 @@ public class UploaderService extends Service {
     }
 
     private void handleIntent(Intent intent) {
-        if (!isOnline(getBaseContext())) {
+		if (!Utils.isOnline(getBaseContext()))
+		{
             return;
         }
+		boolean wifiOnlyUpload = Preferences
+				.isWiFiOnlyUploadActive(getBaseContext());
         UploadsProviderAccessor uploads = new UploadsProviderAccessor(this);
         List<PhotoUpload> pendingUploads = uploads.getPendingUploads();
         for (PhotoUpload photoUpload : pendingUploads) {
@@ -110,6 +125,11 @@ public class UploaderService extends Service {
                 Log.i(TAG, "Upload canceled, because file does not exist anymore.");
                 continue;
             }
+			if (wifiOnlyUpload && !Utils.isWiFiActive(getBaseContext()))
+			{
+				Log.i(TAG, "Upload canceled because WiFi is not active anymore");
+				break;
+			}
             File file = new File(filePath);
             stopErrorNotification(file);
             final Notification notification = showUploadNotification(file);
@@ -155,14 +175,23 @@ public class UploaderService extends Service {
         // TODO adjust this to show the upload manager
         Intent notificationIntent = new Intent(this, MainActivity.class);
         PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
-        Notification notification = new Notification(icon, tickerText, when);
-
         RemoteViews contentView = new RemoteViews(getPackageName(), R.layout.notification_upload);
         contentView.setImageViewResource(R.id.image, icon);
         contentView.setTextViewText(R.id.title, contentMessageTitle);
         contentView.setProgressBar(R.id.progress, 100, 0, true);
-        notification.contentView = contentView;
-        notification.contentIntent = contentIntent;
+		NotificationCompat.Builder builder = new NotificationCompat.Builder(
+				this);
+		Notification notification = builder
+				.setTicker(tickerText)
+				.setWhen(when)
+				.setSmallIcon(icon)
+				.setContentIntent(contentIntent)
+				.setContent(contentView)
+				.getNotification();
+		// Notification notification = new Notification(icon, tickerText, when);
+
+		// notification.contentView = contentView;
+		// notification.contentIntent = contentIntent;
 
         mNotificationManager.notify(NOTIFICATION_UPLOAD_PROGRESS, notification);
 
@@ -199,9 +228,20 @@ public class UploaderService extends Service {
         notificationIntent.putExtra(UploadActivity.EXTRA_PENDING_UPLOAD_URI, photoUpload.getUri());
         PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
 
-        Notification notification = new Notification(icon, titleText, when);
-        notification.flags |= Notification.FLAG_AUTO_CANCEL;
-        notification.setLatestEventInfo(this, titleText, contentMessageTitle, contentIntent);
+		NotificationCompat.Builder builder = new NotificationCompat.Builder(
+				this);
+		Notification notification = builder
+				.setContentTitle(titleText)
+				.setContentText(contentMessageTitle)
+				.setWhen(when)
+				.setSmallIcon(icon)
+				.setAutoCancel(true)
+				.setContentIntent(contentIntent)
+				.getNotification();
+		// Notification notification = new Notification(icon, titleText, when);
+		// notification.flags |= Notification.FLAG_AUTO_CANCEL;
+		// notification.setLatestEventInfo(this, titleText, contentMessageTitle,
+		// contentIntent);
 
         mNotificationManager.notify(file.hashCode(), notification);
     }
@@ -232,9 +272,20 @@ public class UploaderService extends Service {
         }
         PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
 
-        Notification notification = new Notification(icon, titleText, when);
-        notification.flags |= Notification.FLAG_AUTO_CANCEL;
-        notification.setLatestEventInfo(this, titleText, contentMessageTitle, contentIntent);
+		NotificationCompat.Builder builder = new NotificationCompat.Builder(
+				this);
+		Notification notification = builder
+				.setContentTitle(titleText)
+				.setContentText(contentMessageTitle)
+				.setWhen(when)
+				.setSmallIcon(icon)
+				.setAutoCancel(true)
+				.setContentIntent(contentIntent)
+				.getNotification();
+		// Notification notification = new Notification(icon, titleText, when);
+		// notification.flags |= Notification.FLAG_AUTO_CANCEL;
+		// notification.setLatestEventInfo(this, titleText, contentMessageTitle,
+		// contentIntent);
 
         mNotificationManager.notify(file.hashCode(), notification);
     }
@@ -267,23 +318,19 @@ public class UploaderService extends Service {
         }
     }
 
-    private static boolean isOnline(Context context) {
-        try {
-            ConnectivityManager cm = (ConnectivityManager) context
-                    .getSystemService(Context.CONNECTIVITY_SERVICE);
-            return cm.getActiveNetworkInfo().isConnectedOrConnecting();
-        } catch (Exception e) {
-            return false;
-        }
-    }
 
     private class ConnectivityChangeReceiver extends BroadcastReceiver {
 
         @Override
         public void onReceive(Context context, Intent intent) {
-            boolean online = isOnline(context);
+			boolean online = Utils.isOnline(context);
+			boolean wifiOnlyUpload = Preferences
+					.isWiFiOnlyUploadActive(getBaseContext());
             Log.d(TAG, "Connectivity changed to " + (online ? "online" : "offline"));
-            if (online) {
+			if (online
+					&& (!wifiOnlyUpload || (wifiOnlyUpload && Utils
+							.isWiFiActive(context))))
+			{
                 context.startService(new Intent(context, UploaderService.class));
             }
         }
