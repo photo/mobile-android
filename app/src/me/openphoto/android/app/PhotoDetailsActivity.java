896d179bcbe6a9120c818ddd2d07bda643c5f004
Eugene Popovich <httpdispatch@gmail.com>
2012-10-29 09:44:25 +0200

    #114 - implemented viewpager gesture blocking when image is zoomed in the photo details activity. Need to switch to custom GestureImageView repository fork located here https://github.com/httpdispatch/gesture-imageview.git - changed mViewPager type to ViewPagerWithDisableSupport in the PhotoDetailsActivity - added new method isCurrentImageZoomed to the PhotoDetailPagerAdapter inside PhotoDetailsActivity - added setPrimaryItem override to the PhotoDetailPagerAdapter inside PhotoDetailsActivity which stores current view pager view to the variable - added ViewPagerWithDisableSupport - updated layouts activity_photo_details and item_photo_details

--- app/src/me/openphoto/android/app/PhotoDetailsActivity.java
+++ app/src/me/openphoto/android/app/PhotoDetailsActivity.java
@@ -9,6 +9,8 @@ import me.openphoto.android.app.net.ReturnSizes;
 import me.openphoto.android.app.ui.adapter.PhotosEndlessAdapter;
 import me.openphoto.android.app.ui.lib.ImageStorage;
 import me.openphoto.android.app.ui.lib.ImageStorage.OnImageDisplayedCallback;
+import me.openphoto.android.app.ui.widget.ViewPagerWithDisableSupport;
+import me.openphoto.android.app.ui.widget.ViewPagerWithDisableSupport.GesturesEnabledHandler;
 import me.openphoto.android.app.util.GuiUtils;
 import android.app.Activity;
 import android.content.Context;
@@ -25,6 +27,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.polites.android.GestureImageView;
 
 /**
  * The general photo viewing screen
@@ -50,7 +54,7 @@ public class PhotoDetailsActivity extends Activity {
 
     private ImageStorage mStorage;
 
-    private ViewPager mViewPager;
+    private ViewPagerWithDisableSupport mViewPager;
 
     private PhotoDetailPagerAdapter mAdapter;
 
@@ -78,8 +82,15 @@ public class PhotoDetailsActivity extends Activity {
 					photos, tags, album));
         }
 
-        mViewPager = (ViewPager) findViewById(R.id.photos);
+        mViewPager = (ViewPagerWithDisableSupport) findViewById(R.id.photos);
         mViewPager.setAdapter(mAdapter);
+        mViewPager.setGesturesEnabledHandler(new GesturesEnabledHandler() {
+            
+            @Override
+            public boolean isEnabled() {
+                return !mAdapter.isCurrentImageZoomed();
+            }
+        });
 
         int position = getIntent().getIntExtra(EXTRA_ADAPTER_POSITION, 0);
         if (position > 0) {
@@ -91,6 +102,7 @@ public class PhotoDetailsActivity extends Activity {
 
         private final LayoutInflater mInflator;
         private final PhotosAdapter mAdapter;
+        private View mCurrentView;
         private final DataSetObserver mObserver = new DataSetObserver() {
 
             @Override
@@ -191,6 +203,22 @@ public class PhotoDetailsActivity extends Activity {
         public void startUpdate(View arg0) {
         }
 
+        @Override
+        public void setPrimaryItem(ViewGroup container, int position, Object object) {
+            super.setPrimaryItem(container, position, object);
+            mCurrentView = (View) object;
+        }
+
+        boolean isCurrentImageZoomed()
+        {
+            if (mCurrentView != null)
+            {
+                GestureImageView giv = (GestureImageView)
+                        mCurrentView.findViewById(R.id.image);
+                return giv.isZoomed();
+            }
+            return false;
+        }
     }
 
     private class PhotosAdapter extends PhotosEndlessAdapter {
