6805555bd4b9c745364b6b360494e73997ab286f
Eugene Popovich <httpdispatch@gmail.com>
2012-12-05 10:19:19 +0200

    #37 - UploaderService: added external sd mounts file observer. The external SD paths are taken from mount command. - Utils: added experimental method getExternalMounts

--- app/src/me/openphoto/android/app/util/Utils.java
+++ app/src/me/openphoto/android/app/util/Utils.java
@@ -16,6 +16,11 @@
 
 package me.openphoto.android.app.util;
 
+import java.io.InputStream;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
+
 import me.openphoto.android.app.R;
 import android.content.Context;
 import android.net.ConnectivityManager;
@@ -91,5 +96,54 @@ public class Utils {
     public static boolean isTablet(Context context)
     {
         return context.getResources().getBoolean(R.bool.isTablet);
+    }
+
+    /**
+     * Returns possible external sd card path. Solution taken from here
+     * http://stackoverflow.com/a/13648873/527759
+     * 
+     * @return
+     */
+    public static Set<String> getExternalMounts() {
+        final Set<String> out = new HashSet<String>();
+        try
+        {
+            String reg = ".*vold.*(vfat|ntfs|exfat|fat32|ext3|ext4).*rw.*";
+            StringBuilder sb = new StringBuilder();
+            try {
+                final Process process = new ProcessBuilder().command("mount")
+                        .redirectErrorStream(true).start();
+                process.waitFor();
+                final InputStream is = process.getInputStream();
+                final byte[] buffer = new byte[1024];
+                while (is.read(buffer) != -1) {
+                    sb.append(new String(buffer));
+                }
+                is.close();
+            } catch (final Exception e) {
+                e.printStackTrace();
+            }
+            // parse output
+            final String[] lines = sb.toString().split("\n");
+            for (String line : lines) {
+                if (!line.toLowerCase(Locale.ENGLISH).contains("asec")) {
+                    if (line.matches(reg)) {
+                        String[] parts = line.split(" ");
+                        for (String part : parts) {
+                            if (part.startsWith("/"))
+                                if (!part.toLowerCase(Locale.ENGLISH).contains("vold"))
+                                {
+                                    CommonUtils.debug(TAG, "Found path: " + part);
+                                    out.add(part);
+                                }
+                        }
+                    }
+                }
+            }
+        } catch (Exception ex)
+        {
+            GuiUtils.noAlertError(TAG, ex);
+        }
+        return out;
     }
 }
